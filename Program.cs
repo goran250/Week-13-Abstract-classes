9e4cbc5f8dcea7377a15c6575f56fa6f1abec61d
goran131 <goran.rosenberg@tomelilla.nu>
2026-03-24 09:31:07 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Abstract_classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // abstract classes = modifier that indicates missing or incomplete implementation
+            Console.WriteLine("Abstract Classes & Abstract Methods");
+
+            Car car = new Car();
+            Bicycle bicycle = new Bicycle();
+            Boat boat = new Boat();
+
+            Console.ReadLine();
+        }
+    }
+
+    // base class - abstract class
+    abstract class Vehicle
+    {
+        public int speed = 0;
+
+        // Regular method or Non-abstract method
+        public void Go()
+        {
+            Console.WriteLine("This Vehicle is Moving");
+        }
+    }
+
+    // subclasses of Vehicle
+    class Car : Vehicle
+    {
+        public int wheels = 4;
+        int maxSpeed = 200;
+
+    }
+
+    class Bicycle : Vehicle
+    {
+        public int wheels = 2;
+        int maxSpeed = 50;
+
+    }
+
+    class Boat : Vehicle
+    {
+        public int wheels = 0;
+        int maxSpeed = 100;
+
+    }
+}
